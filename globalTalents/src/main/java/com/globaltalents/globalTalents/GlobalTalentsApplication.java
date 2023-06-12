8287ee2288a6ea9a8916a3af7eafca5fd75f9ea7
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-06-12 11:30:11 +0530

    Initial Code

---
+++ globalTalents/src/main/java/com/globaltalents/globalTalents/GlobalTalentsApplication.java
@@ -0,0 +1,13 @@
+package com.globaltalents.globalTalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTalentsApplication.class, args);
+	}
+
+}
---
+++ globalTalents/src/test/java/com/globaltalents/globalTalents/GlobalTalentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaltalents.globalTalents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTalentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
